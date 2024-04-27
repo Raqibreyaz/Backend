4388dc7d86b92b5a4c5e445a8cd9a4e1cc2bead3
Raqibreyaz <raquibreyaz111@gmail.com>
2024-04-27 15:33:32 +0530

    Learnt MiddleWares

--- ExpressStart/server.js
+++ ExpressStart/server.js
@@ -1,2 +1,52 @@
 const express = require('express')
 
+const app = express();
+
+// middleware will run before hitting any route
+app.use((req, res, next) => {
+    console.log('middleware chala');
+    next();
+}
+)
+
+// this middleware will run after the previous middleware passes the request
+app.use((req, res, next) => {
+    console.log('middleware chala phir se');
+    next();
+}
+)
+
+app.get('/', (req, res) => {
+    console.log('finally / route hit kiya');
+
+    res.send('hi buddy')
+}
+)
+
+app.get('/about', (req, res) => {
+    console.log('finally /about route hit kiya');
+
+    res.send('hi')
+}
+)
+
+app.get('/profile', (req, res, next) => {
+
+    console.log('in the route');
+
+    // passing error to midleware
+    return next(new Error("Not Implemented"))
+}
+)
+
+// this type of middleware runs only on errors
+app.use((error, req, res, next) => {
+    console.log('in a middleware');
+
+res.status(404).send(error.message)
+    
+    next();
+}
+)
+
+app.listen(3000);
